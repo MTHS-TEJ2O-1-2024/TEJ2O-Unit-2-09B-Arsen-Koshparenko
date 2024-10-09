74099f0925d145f8f2f74ac4b2df2cc1fe34a953
Arsen-Koshparenko <arsen.koshparenko@mths.ca>
2024-10-09 16:35:55 -0400

    game is work :)

--- main.ts
+++ main.ts
@@ -1,8 +1,52 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Arsen
+ * Created on: Oct 2024
+ * This program play rock, paper, scissors
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = 0
+let totalPoint = 0
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+    // if randomNumber was 0
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+        pause(2000)
+        basic.clearScreen()
+        basic.showIcon(IconNames.Happy)
+    }
+
+    // if randomNumber was 1
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.Square)
+        pause(2000)
+        basic.clearScreen()
+        basic.showIcon(IconNames.Happy)
+    }
+    // if randomNumber was 2
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+        pause(2000)
+        basic.clearScreen()
+        basic.showIcon(IconNames.Happy)
+    }
+})
+
+input.onButtonPressed(Button.A, function () {
+    totalPoint += 1
+    basic.showIcon(IconNames.Yes)
+    pause(1000)
+    basic.clearScreen()
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Score is: " + totalPoint)
+})
